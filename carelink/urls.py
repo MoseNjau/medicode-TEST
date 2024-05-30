eff825f99c7b131c89ad400fe9c2377716d49b50
GEO-HACK <geoffreykirumba0@gmail.com>
2024-05-30 23:48:38 +0300

    profile page

--- carelink/urls.py
+++ carelink/urls.py
@@ -1,6 +1,8 @@
 from os import name
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,5 +25,5 @@ urlpatterns = [
     path('search_service_provider/',views.search_service_provider,name="search_provider"),
     path('get_doctor_in_hospital/',views.get_doctor_in_hospital,name="get_doctor_in_hospital"),
     path('search_doctor/',views.search_doctor,name="search_doctor")
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
